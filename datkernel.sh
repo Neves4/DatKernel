1cf4366caf95a61dca6dd39afab440ed7ebd38a8
Neves4 <victorneves478@gmail.com>
2015-10-17 11:44:15 -0300

    Updated my build script, everything is automated now!!
    Ready for v13r2, yay :)

--- datkernel.sh
+++ datkernel.sh
@@ -1,21 +1,27 @@
-# Start Compilation
-clear
+clear										# Start Compilation
+
+echo "==============================================="
 echo "Giving executing permissions to clean script"
 echo "==============================================="
-echo -e ""
-chmod +x clean.sh
+
+if [ -a arch/arm/boot/zImage ]; 						# If it's already build, clean everything
+then
+
 ./clean.sh
 echo -e ""
-echo -e ""
-read -p "Cleaning done Enter to proceed ..."
+echo -e "==============================================="
+echo -e "Cleaning done"
 echo -e "==============================================="
 echo -e ""
 clear
 
-read -p "Initialize Toolchain by pressing ENTER ..."
+else										#If not, continue
+
+echo -e "==============================================="
+echo -e "Initializing Toolchain"
 echo -e "==============================================="
 
-# Show stuff to compiler
+fi 										# Show stuff to compiler
 
 export ARCH=arm
 export SUBARCH=arm
@@ -25,8 +31,7 @@ echo "Done"
 echo "==============================================="
 echo -e ""
 
-# make config
-make datkernel_defconfig
+make datkernel_defconfig							# make config
 
 echo -e ""
 echo "==============================================="
@@ -34,8 +39,75 @@ echo "Done"
 echo "==============================================="
 echo -e ""
 
-read -p "Press Enter to build Kernel ..."
+echo -e "Build Kernel ..."
 echo "==============================================="
 echo -e ""
+clear
 
-make -j4
+time make -j4
+
+echo "==============================================="
+echo 'copying files to ./out'
+echo "==============================================="
+echo ''
+
+mkdir -p out/modules                                                       	# make dirs for zImage and modules
+cp arch/arm/boot/zImage out/zImage                                        	# copy zImage
+
+find -name '*.ko' | xargs -I {} cp {} ./out/modules/				# Find and copy modules
+cp -r out/modules/*.ko ~/android/kernel/Saida/codekidX/lib/modules/
+cp -r out/zImage ~/android/kernel/Saida/kernel/                           	# copy zImage and modules to Saida
+
+echo "==============================================="
+echo 'done'
+echo "==============================================="
+echo ''
+
+if [ -a arch/arm/boot/zImage ]; 
+then										# If zImage is done, continue	
+
+echo "==============================================="
+echo 'Copy empty-zip'
+echo "==============================================="
+echo ''
+
+cp ~/android/kernel/empty-zip.zip ~/android/kernel/Saida/boot.zip		# Copy the empty zip to the working folder
+
+echo ''
+echo "==============================================="
+echo 'Copying modules to zip'
+echo "==============================================="
+echo ''
+
+cd ~/android/kernel/Saida/							# Change to working folder 
+shopt -s globstar								# list everything on the folder (including structures)
+zip -u boot.zip ./codekidX/lib/modules/*.ko					# Copy all modules to zip (update)
+zip -u boot.zip ./kernel/zImage							# Copy zImage (update)
+mv boot.zip ~/android/kernel/DatKernel-last.zip					# move last to main folder, and all working folders are now clean
+                                                            
+echo ''
+echo "==============================================="
+echo 'build finished successful, cleaning everything'
+echo "==============================================="
+
+cd codekidX/lib/modules/							# Cleaning all modules after zip is done
+rm *
+
+cd ~/android/kernel/Saida/kernel/						# Cleaning zImage as well
+rm *
+
+cd ~/android/kernel/DatKernel							# Going to initial folder,
+./clean.sh									# cleaning all build stuff,	
+rm -rf out									# and clean out folder and wipe it
+
+git checkout -- drivers/misc/vc04_services/interface/vchiq_arm/vchiq_version.c	# Checkout that fucker
+
+else										# if zImage was not done, build failed
+
+echo "==============================================="
+echo 'build failed!'
+echo "==============================================="
+
+make -j1									# Find where the build failed.
+
+fi
