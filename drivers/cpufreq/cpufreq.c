Neves4/DatKernel

commit c2fc14969ba1e84345709a4e2c571ebd0daf904d

cpufreq: Only call cpufreq_out_of_sync() for driver that implement cpufreq_driver.target()
Scaling drivers that implement cpufreq_driver.setpolicy() have
internal governors that do not signal changes via
cpufreq_notify_transition() so the frequncy in the policy will almost
certainly be different than the current frequncy.  Only call
cpufreq_out_of_sync() when the underlying driver implements
cpufreq_driver.target()

Signed-off-by: Dirk Brandewie <dirk.j.brandewie@intel.com>
Acked-by: Viresh Kumar <viresh.kumar@linaro.org>
Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
Signed-off-by: Blechd0se <alex.christ@hotmail.de>
Signed-off-by: Eliminater74 <eliminater74@gmail.com>

drivers/cpufreq/cpufreq.c
@@ -1738,7 +1738,7 @@ int cpufreq_update_policy(unsigned int cpu)
 			pr_debug("Driver did not initialize current freq");
 			data->cur = policy.cur;
 		} else {
-			if (data->cur != policy.cur)
+			if (data->cur != policy.cur && cpufreq_driver->target)
 				cpufreq_out_of_sync(cpu, data->cur,
 								policy.cur);
 		}

commit b83dde88e240fe4655df2c13d640196a8f115685

Damn, cpufreq was broken since v13r2-beta2. Fixed it right now!
The commit: c2fc14969ba1e84345709a4e2c571ebd0daf904d broke my cpufreq and it never updated the cpufreq. So, all the time my clock was at 1200MHz, and felt very smooth, but I wasn't using my governor at all (only 2 freqs: Deep Sleep and 1200MHz). So fixed this commit manually, and now everything is flying again.

drivers/cpufreq/cpufreq.c
@@ -1752,7 +1752,7 @@ int cpufreq_update_policy(unsigned int cpu)
 			pr_debug("Driver did not initialize current freq");
 			data->cur = policy.cur;
 		} else {
-			if (data->cur != policy.cur && cpufreq_driver->target)
+			if (data->cur != policy.cur)
 				cpufreq_out_of_sync(cpu, data->cur,
 								policy.cur);
 		}