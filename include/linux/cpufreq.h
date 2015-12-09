09d24369d300e8f4f6b82fb91fcad774aefa02a3
Neves4 <victorneves478@gmail.com>
2015-12-09 16:21:51 -0200

    Enable Bioshock and DanceDance, while removing Despair governor for now

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -390,9 +390,6 @@ extern struct cpufreq_governor cpufreq_gov_dancedance;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BIOSHOCK)
 extern struct cpufreq_governor cpufreq_gov_bioshock;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_bioshock)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DESPAIR)
-extern struct cpufreq_governor cpufreq_gov_despair;
-#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_despair)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ALUCARD)
 extern struct cpufreq_governor cpufreq_gov_alucard;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_alucard)
